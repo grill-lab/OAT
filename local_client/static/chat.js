aa71152099aa9ce1615f044de2311d5b4f38b23c
Chinwekele Izuzu <chinwekele@Chinwekeles-MBP.home>
2022-08-06 10:28:11 +0100

    Video updates

---
+++ local_client/static/chat.js
@@ -0,0 +1,17 @@
+var coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+    });
+}
+
--- local_client/static/scripts.js
+++ local_client/static/scripts.js
@@ -1,5 +1,3 @@
-
-
 encodeDictInURI = (dict) => {
     uri = ""
     var esc = encodeURIComponent;
@@ -12,10 +10,110 @@ encodeDictInURI = (dict) => {
 
 }
 
+const searchForm = document.querySelector("#search-form");
+const searchFormInput = document.querySelector("input");
+
+const speechForm = document.querySelector("#speechbar")
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "en";
+
+if (SpeechRecognition){
+    console.log("Your browser supports sr");
+
+    // adding the mic button if browser supports
+    // searchForm.insertAdjacentHTML("afterbegin", '<button type="button" id="speechbutton"><i class="fas fa-microphone"></i></button>');
+    const micBtn = speechForm.querySelector("button");
+    const micIcon = micBtn.querySelector("i");
+
+    const recognition = new SpeechRecognition();
+    recognition.continous = true;
+
+
+    micBtn.addEventListener("click", micBtnClick);
+    function micBtnClick () {
+        if (micIcon.classList.contains("fa-microphone")) {
+            recognition.start();
+        }
+        else {
+            
+            recognition.stop();
+        }
+    }
+    recognition.addEventListener("start", startSpeechRecognition);
+    function startSpeechRecognition() {
+        micIcon.classList.remove("fa-microphone");
+        micIcon.classList.add("fa-microphone-slash");
+        searchFormInput.focus();
+        console.log("Speech recognition started");
+    }
+
+    recognition.addEventListener("end", endSpeechRecognition);
+    function endSpeechRecognition() {
+        micIcon.classList.add("fa-microphone");
+        micIcon.classList.remove("fa-microphone-slash");
+        searchFormInput.focus();
+        console.log("Speech recognition ended");
+    }
+
+    recognition.addEventListener("result", resultOfSpeechRecognition); //<==> recognition.onresult = function(event) {....}
+    function resultOfSpeechRecognition(event) {
+        console.log(event);
+        const currentResultIndex = event.resultIndex
+        const transcript = event.results[currentResultIndex][0].transcript;
+        searchFormInput.value = transcript;
+
+        const call = async () => {
+
+            data = {
+                text : searchFormInput.value||document.getElementById('textInput').value,
+                id   : document.getElementById('IDField').value,
+                headless: 'false',
+            }
+        
+            const response = await fetch('/getresponse?'+ encodeDictInURI(data));
+            const myJson = await response.json();
+
+            let speech = new SpeechSynthesisUtterance();
+            speech.lang = "en";
+
+            speech.text = myJson['speechText'];
+            speech.rate = 0.009;
+            window.speechSynthesis.speak(speech);
+        
+            myJson['speechText'] = myJson['speechText'].replace(/[\.\:] /g, "<br/>")
+            console.log(myJson)
+
+            display("User", searchFormInput.value)
+            display("BOT", myJson['speechText'])
+
+            renderScreen(myJson['screen'], myJson['template'])
+            document.getElementById('textInput').value = ''
+        }
+
+        if (transcript.toLowerCase().trim()=="stop recording") {
+            recognition.stop();
+        }
+        else if (!searchFormInput.value) {
+            searchFormInput.value = transcript;
+        }
+        else {
+            setTimeout(() => {
+                call();
+            }, 150);
+        }
+    }
+}
+else {
+    console.log("your browser does not support");
+}
+
+
 const call = async () => {
 
     data = {
-        text : document.getElementById('inputText').value,
+        text : document.getElementById('textInput').value,
         id   : document.getElementById('IDField').value,
         headless: 'false',
     }
@@ -23,13 +121,20 @@ const call = async () => {
     const response = await fetch('/getresponse?'+ encodeDictInURI(data));
     const myJson = await response.json();
 
+    let speech = new SpeechSynthesisUtterance();
+            speech.lang = "en";
+
+            speech.text = myJson['speechText'];
+            speech.rate = 0.009;
+            window.speechSynthesis.speak(speech);
+
     myJson['speechText'] = myJson['speechText'].replace(/[\.\:] /g, "<br/>")
     console.log(myJson)
-    display("User", document.getElementById('inputText').value)
+    display("User", document.getElementById('textInput').value)
     display("BOT", myJson['speechText'])
 
     renderScreen(myJson['screen'], myJson['template'])
-    document.getElementById('inputText').value = ''
+    document.getElementById('textInput').value = ''
 }
 
 const renderScreen = (screen_interaction, extra_template) => {
@@ -108,7 +213,7 @@ const renderScreen = (screen_interaction, extra_template) => {
         for (let i = 0, len = screen_interaction['buttons'].length; i < len; i++) {
             buttons[i].innerText += screen_interaction["buttons"][i]
             buttons[i].addEventListener('click', function () {
-                document.getElementById('inputText').value = screen_interaction["onClickList"][i];
+                document.getElementById('textInput').value = screen_interaction["onClickList"][i];
                 call();
             })
             buttons[i].style.display = ""
@@ -118,11 +223,19 @@ const renderScreen = (screen_interaction, extra_template) => {
 
 const display = (user, text) => {
 
-    chatNode = document.getElementsByClassName("chat")[0]
-    chatNode.innerHTML += "<p> <strong>" + user + "</strong>: " + text + "</p>";
+    if (user=="BOT") {
+        chatNode = document.getElementsByClassName("chat")[0]
+        chatNode.innerHTML += "<p id='botStarterMessage'><span>" + text + "</span></p>";
 
-    chatNode.scrollTop= chatNode.scrollHeight
+        chatNode.scrollTop= chatNode.scrollHeight
+    }
 
+    else {
+        chatNode = document.getElementsByClassName("chat")[0]
+        chatNode.innerHTML += "<p id='usermessage'><span>" + text + "</span></p>";
+
+        chatNode.scrollTop= chatNode.scrollHeight
+    }
 }
 
 const assignID = () => {
